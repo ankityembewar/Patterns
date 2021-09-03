ba88186008dd2c9f1eaac881adf46a753c6e0d2b
ankityembewar <71712601+ankityembewar@users.noreply.github.com>
2021-09-03 13:15:25 +0530

    Create Right_Triangle.cs

---
+++ Patterns/Right_Triangle.cs
@@ -0,0 +1,16 @@
+using System;
+class Right_Pyramid
+{
+  static void Main() 
+  {
+      
+     for(int i=1;i<=7;i++)
+     {
+         for(int j=1;j<=i;j++)
+         {
+             Console.Write("*");
+         }
+         Console.WriteLine();
+     }
+  }
+}
